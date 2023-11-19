f078a7df5926269ce409f831b158fc1113f29820
Pieter Koornhof <koornhof.pieter@gmail.com>
2023-11-19 10:23:36 +0200

    Chore: basic firestore example

--- src/main/example.cljs
+++ src/main/example.cljs
@@ -1,29 +1,93 @@
 (ns main.example
   (:require [reagent.core :as r]
             [main.firebase.config :as firebase]
-            ["firebase/auth" :as firebase-auth]))
+            ["firebase/auth" :as firebase-auth]
+            ["firebase/firestore" :as firebase-firestore]))
 
 
-(defonce ^:private *app-state (r/atom {:logged-in? false}))
+(defonce ^:private *app-state (r/atom {:logged-in? false
+                                       :items {}}))
+
+
+;; DB
+(defonce ^:private *collection-unsub (atom nil))
+
+(def ^:private items-collection (firebase-firestore/collection firebase/firestore "items"))
+
+
+(defn- update-from-sub [change]
+  (let [change-type (.-type change)
+        id (.. change -doc -id)]
+    (cond
+      (contains? #{"added" "modified"} change-type)
+      (swap! *app-state assoc-in [:items id] (js->clj (.data (.-doc change))))
+
+      (= "removed" change-type)
+      (swap! *app-state update :items dissoc id))))
+
+
+(defn- subscribe! []
+  (let [q     (firebase-firestore/query items-collection)
+        unsub (firebase-firestore/onSnapshot
+                q
+                (fn [snapshot]
+                  (doseq [change (.docChanges snapshot)]
+                    (update-from-sub change))))]
+    (reset! *collection-unsub unsub)))
+
+
+(defn- unsubscribe! []
+  (when-let [unsub @*collection-unsub]
+    (prn "unsub")
+    (unsub)
+    (reset! *collection-unsub nil)))
+
+
+(defn- insert-random-item! []
+  (firebase-firestore/addDoc items-collection (clj->js {:value (rand-int 10000)})))
+
+(defn- remove-item! [id]
+  (firebase-firestore/deleteDoc
+    (firebase-firestore/doc firebase/firestore "items" id)))
 
 ;; EVENTS
 
 (defn- log-in! []
   (-> (firebase-auth/signInAnonymously firebase/auth)
-      (.then #(swap! *app-state assoc :logged-in? true))
+      (.then (fn []
+               (subscribe!)
+               (swap! *app-state assoc :logged-in? true)))
       (.catch #(js/alert "Make sure firebase emulators are running"))))
 
 
 (defn- log-out! []
   (-> (firebase-auth/signOut firebase/auth)
-      (.then #(swap! *app-state assoc :logged-in? false))))
+      (.then (fn []
+               (unsubscribe!)
+               (swap! *app-state assoc :logged-in? false)))))
+
+
+(defn- add-entity! []
+  (-> (insert-random-item!)
+      (.catch #(js/alert "Make sure firebase emulators are running"))))
+
+(defn- remove-entity! [id]
+  (-> (remove-item! id)
+      (.catch #(js/alert "Make sure firebase emulators are running"))))
 
 ;; VIEWS
 
-(defn- logged-in-view []
+(defn- logged-in-view [app-state]
   [:div
    [:h3 "Welcome"]
-   [:button {:on-click log-out!} "Log Out"]])
+   [:button.paper-btn.btn-primary {:on-click add-entity!} "Add random entity"]
+   [:button {:on-click log-out!} "Log Out"]
+   [:ul
+    (->> (:items app-state)
+         (map (fn [[id item]]
+                [:li {:key id}
+                 (get item "value")
+                 [:a {:on-click #(remove-entity! id)}" (remove)"]])))]])
 
 (def ^:private logged-out-view
   [:div
